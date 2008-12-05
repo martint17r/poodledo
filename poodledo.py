a4ca9d1a84315573d4a729aab79d0426e004c944
felix.riedel <felix.riedel@2712e28e-bff2-11dd-9405-3d0920048215>
2008-12-05 20:22:16 +0000

    Tried to clean up code before adding new stuff
    git-svn-id: http://poodledo.googlecode.com/svn/trunk@4 2712e28e-bff2-11dd-9405-3d0920048215

--- poodledo.py
+++ poodledo.py
@@ -5,18 +5,15 @@ import os
 import urllib
 import urllib2
 
-import pickle
+import elementtree.ElementTree as ET
 import time
 from datetime import datetime, timedelta
-import elementtree.ElementTree as ET
 from md5 import md5
 
-# import logging
-# logging.basicConfig()
-# LOG = logging.getLogger(__name__)
-# LOG.setLevel(logging.DEBUG)
+__all__ = ['ApiClient']
 
 class ToodledoError(Exception):
+    ''' Error return from Toodledo API server'''
 
     def __init__(self, error_msg):
         self.msg = error_msg
@@ -24,86 +21,34 @@ class ToodledoError(Exception):
     def __str__(self):
         return "Toodledo server returned error: %s" % self.msg
 
-class Token(object):
-    def __init__(self, email, userid, token, expires = None):
-        self.email = email
-        self.userid = userid
-        self.token = token
-        if not(expires):
-            self.tokenexpires = (datetime.datetime.now() + datetime.timedelta(hours=3, minutes=30))
-        else:
-            self.tokenexpires = expires
-
-class TokenCache(object):
-        _TOKEN_FILE = '~/.toodledo_tokencache'
-
-        def __init__(self):
-            filename = os.path.expanduser(TokenCache._TOKEN_FILE)
-            if os.path.exists(filename):
-                f = open(filename, 'r')
-                self.cache = pickle.load(f)
-                self.userids = pickle.load(f)
-                f.close()
-            else:
-                self.cache = {}
-                self.userids = {}
-
-        def saveCache(self):
-            filename = os.path.expanduser(TokenCache._TOKEN_FILE)
-            f = open(filename, 'w')
-            pickle.dump(self.cache, f)
-            pickle.dump(self.userids, f)
-            f.close()
-
-
-        def addToken(self, token):
-            self.cache[token.userid] = token
-            self.userids[token.email] = token.userid
-            self.saveCache()
-
-
-def local_date(string):
-    dt = datetime.strptime(string[0:25], '%a, %d %b %Y %H:%M:%S')
-    return dt + timedelta(hours=6) + timedelta(seconds=local_time_offset())
-
-def local_time_offset():
-    """Return offset of local zone from GMT"""
-    if time.localtime().tm_isdst and time.daylight:
-        return -time.altzone
-    else:
-        return -time.timezone
-
-
-def boolstr(string):
-    return bool(int(string))
 
 class ToodledoData(object):
     _typemap = {
             'server': {
                 'unixtime': int,
-                'date': local_date,
+                'date': _local_date,
                 'tokenexpires': float
                 },
             'folder': {
                 'id': int,
-                'archived': boolstr,
-                'private': boolstr,
+                'archived': _boolstr,
+                'private': _boolstr,
                 'order': int
                 },
             'context': {
                 'id': int,
-                'default': boolstr,
+                'default': _boolstr,
                 },
             'goal': {
                 'id': int,
                 'level': int,
                 'contributes': int,
-                'archived': boolstr
+                'archived': _boolstr
                 },
             'account': {
                 'userid': str,
                 'alias': str,
-                'pro': boolstr,
+                'pro': _boolstr,
                 'dateformat': int,
                 'timezone': int,
                 'hidemonths': int,
@@ -147,18 +92,38 @@ class ApiClient(object):
         self._urlopener = opener
 
 
-    def _call(self, **kwargs):
-        assert('method' in kwargs)
-        url = self._create_url(**kwargs)
-        return self._urlopener.open(url)
-
-
     def _create_url(self,**kwargs):
+        ''' Creates a request url by appending key-value pairs to the SERVICE_URL'''
         url = ApiClient._SERVICE_URL
         # add args to url (key1=value1;key2=value2)
         # trailing underscores are stripped from keys to allow keys like pass_
         url += ';'.join(key.rstrip('_') + '=' + kwargs[key] for key in sorted(kwargs))
         return url
+
+    def _call(self, **kwargs):
+        assert('method' in kwargs)
+        url = self._create_url(**kwargs)
+        return self._urlopener.open(url)
+
+    def _check_for_error(self, node):
+        if node.tag == 'error':
+            raise ToodledoError(node.text)
+
+    def _get_call(self,method,key=None, **kwargs):
+        if key is None:
+            key = self.key
+        stream = self._call(method=method, key=key, **kwargs)
+        root_node = ET.parse(stream).getroot()
+        self._check_for_error(root_node)
+        return ToodledoData(root_node)
+
+    def _get_list_call(self,method,key=None,**kwargs):
+        if key is None:
+            key = self.key
+        stream = self._call(method=method, key=key, **kwargs)
+        root_node = ET.parse(stream).getroot()
+        self._check_for_error(root_node)
+        return [ ToodledoData(elem) for elem in root_node.getchildren() ]
 
     def authenticate(self, email, passwd):
         ''' Uses credentials to get userid, token and auth key'''
@@ -174,16 +139,11 @@ class ApiClient(object):
         ''' Generates a key as specified in the API docs'''
         return md5(md5(passwd).hexdigest() + token + userid).hexdigest()
 
-
     def getUserid(self, email, passwd):
         stream = self._call(method='getUserid', email=email, pass_=passwd)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
-
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
-        else:
-            return root_node.text
+        root_node = ET.parse(stream).getroot()
+        self._check_for_error(root_node)
+        return root_node.text
 
     def getToken(self,userid=None):
         if userid is None:
@@ -191,67 +151,37 @@ class ApiClient(object):
                 userid = self.userid
             else:
                 raise Exception() # TODO: 
-
-        stream = self._call(method='getToken', userid=userid)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
-
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
-        else:
-            return root_node.text
+        root_node = ET.parse(self._call(method='getToken', userid=userid)).getroot()
+        self._check_for_error(root_node)
+        return root_node.text
 
     def getServerInfo(self,key=None):
-        if key is None:
-            key = self.key
-        stream = self._call(method='getServerInfo', key=key)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
-
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
-
-        return ToodledoData(root_node) 
-
-    def getFolders(self,key=None):
-        if key is None:
-            key = self.key
-        stream = self._call(method='getFolders', key=key)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
-
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
-
-        folders = []
-        for elem in root_node.getchildren():
-            folders.append( ToodledoData(elem))
-        return folders
-
-    def getContexts(self,key=None):
-        if key is None:
-            key = self.key
-        stream = self._call(method='getContexts', key=key)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
-
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
-
-        contexts = []
-        for elem in root_node.getchildren():
-            contexts.append( ToodledoData(elem))
-        return contexts
+        return self._get_call(method='getServerInfo', key=key)
 
     def getAccountInfo(self,key=None):
-        if key is None:
-            key = self.key
-        stream = self._call(method='getAccountInfo', key=key)
-        xmltree = ET.parse(stream)
-        root_node = xmltree.getroot()
+        return self._get_call(method='getAccountInfo', key=key)
 
-        if root_node.tag == 'error':
-            raise ToodledoError(root_node.text)
 
-        return ToodledoData(root_node)
+    def getFolders(self,key=None):
+        return self._get_list_call('getFolders',key=key)
 
+    def getContexts(self,key=None):
+        return self._get_list_call('getContexts',key=key)
+
+    def getGoals(self,key=None):
+        return self._get_list_call('getGoals',key=key)
+
+
+def _local_date(string):
+    dt = datetime.strptime(string[0:25], '%a, %d %b %Y %H:%M:%S')
+    return dt + timedelta(hours=6) + timedelta(seconds=_local_time_offset())
+
+def _local_time_offset():
+    """Return offset of local zone from GMT"""
+    if time.localtime().tm_isdst and time.daylight:
+        return -time.altzone
+    else:
+        return -time.timezone
+
+def _boolstr(string):
+    return bool(int(string))
